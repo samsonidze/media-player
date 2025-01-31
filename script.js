90487eb1bcca0505378e46964e2aaf1175b1424a
Giorgi Samsonidze <119052018+samsonidze@users.noreply.github.com>
2025-01-31 15:14:25 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -62,3 +62,18 @@ function playPrev() {
     ctrlIcon.classList.add("fa-pause");
     h1.textContent = "Mockingbird"
 }
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  if (menu.style.maxHeight) {
+    menu.style.maxHeight = null; 
+    menu.style.opacity = 0; 
+  } else {
+    menu.style.maxHeight = menu.scrollHeight + 'px'; 
+    menu.style.opacity = 1; 
+  }
+});
+
